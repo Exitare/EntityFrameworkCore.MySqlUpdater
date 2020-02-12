178473b994aac6879e3f7307abf184b9dd8222e4
Exitare <business.exitare@icloud.com>
2020-02-12 15:58:04 -0800

    Initial commit.

---
+++ MySQLDBUpdater/MySQLDBUpdater/HashSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MySQLDBUpdater
+{
+    public class HashSum
+    {
+
+        /// <summary>
+        /// Creates a SHA1 Hashsum 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static string CreateSHA1Hash(string content)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = new SHA1Managed().ComputeHash(Encoding.UTF8.GetBytes(content));
+                return string.Concat(hash.Select(b => b.ToString("x2")));
+
+            }
+        }
+    }
+}
