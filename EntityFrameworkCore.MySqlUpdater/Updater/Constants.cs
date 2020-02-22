52fac64cdf92d5e079358243d3c802d9f4538f30
Exitare <business.exitare@icloud.com>
2020-02-21 22:40:34 -0800

    Updated update statuscodes

--- EntityFrameworkCore.MySqlUpdater/Updater/Constants.cs
+++ EntityFrameworkCore.MySqlUpdater/Updater/Constants.cs
@@ -6,15 +6,14 @@ namespace EntityFrameworkCore.MySqlUpdater
 {
     public enum UpdateStatusCodes
     {
-        SUCCESS = 0x10,
-        NOT_A_SQL_FILE = 0x20,
-        INSECURE_SQL_QUERY = 0x30,
-        EMPTY_CONTENT = 0x40,
-        UPDATE_ALREADY_APPLIED = 0x50,
-        MAX_ALLOWED_PACKAGE_LIMIT_TOO_SMALL = 0x60,
-        SCHEMA_NOT_EMPTY = 0x70,
-        UPDATE_TABLE_MISSING = 0x80,
-        FILE_NOT_FOUND = 0x90,
+        SUCCESS = 0x00,
+        NOT_A_SQL_FILE = 0x10,
+        INSECURE_SQL_QUERY = 0x20,
+        EMPTY_CONTENT = 0x30,
+        UPDATE_ALREADY_APPLIED = 0x40,
+        SCHEMA_NOT_EMPTY = 0x50,
+        UPDATE_TABLE_MISSING = 0x60,
+        FILE_NOT_FOUND = 0x70,
     }
 
     public enum SHAStatus
